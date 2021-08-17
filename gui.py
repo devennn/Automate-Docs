d81da2e6e9ad445f3ba4abaf89f13c507febe605
devennn <myinsp98@gmail.com>
2021-08-17 23:28:37 +0800

    Update GUI

--- gui.py
+++ gui.py
@@ -1,6 +1,7 @@
 import PySimpleGUI as sg
 from selector.selector_opencv import run_selector
 from template import make_template, is_template_name_valid, get_template_name_list
+from ocr.ocr import run_ocr
 
 sg.theme('Dark Blue 3')
 
@@ -10,11 +11,14 @@ def get_window():
     tab_1 = [[sg.Text("User Choose template, raw image source, cropped image dest, name of output file")],
              [sg.Text("Choose Template", size=(15, 1)),
               sg.Combo(template_name_list, size=(45, 1), key='-TEMPLATE_CHOICE-')],
-             [sg.Text("Image Source", size=(15, 1)), sg.InputText(key='-FILE1-'), sg.FileBrowse(target='-FILE1-')],
-             [sg.Submit(size=(10, 1))]]
+             [sg.Text("Image Source", size=(15, 1)), sg.InputText(key='-FOLDER1-'), sg.FolderBrowse(target='-FOLDER1-')],
+             [sg.Button("Start", size=(10, 1), key="-START_OCR-")]]
+
     tab_2 = [[sg.Text("Choose Image", size=(15, 1)), sg.InputText(key='-FILE2-'), sg.FileBrowse(target='-FILE2-')],
              [sg.Text("Template Name", size=(15, 1)), sg.InputText(key='-TEMPLATE_NAME-')],
-             [sg.Button(button_text="Start", size=(10, 1), key="-start_templates-")]]
+             [sg.Button(button_text="Start", size=(10, 1), key="-start_templates-"),
+              sg.Text("", key="-TEMPLATE_TAB_OUTPUT-")]]
+
     layout = [[sg.TabGroup([[sg.Tab("Perform OCR", tab_1), sg.Tab("Make Templates", tab_2)]])]]
 
     return sg.Window("Document OCR", layout)
@@ -34,15 +38,20 @@ def main():
         # Change state
         if event == "-start_templates-":
             state = "RUN_TEMPLATE"
+        elif event == "-START_OCR-":
+            state = "RUN_OCR"
 
         # Run state
         if state == "RUN_TEMPLATE" and values['-FILE2-'] != "":
             if is_template_name_valid(values['-TEMPLATE_NAME-']):
                 crop_values = run_selector(values['-FILE2-'])
                 make_template(crop_values, values['-TEMPLATE_NAME-'])
+                window.Element("-TEMPLATE_TAB_OUTPUT-").Update(value="Template created: {}".format(
+                    values['-TEMPLATE_NAME-']))
+                window.Element('-TEMPLATE_CHOICE-').Update(values=get_template_name_list())
             state = "WAITING"
-        elif state == "RUN_OCR":
-            print("TO DO: RUN OCR")
+        elif state == "RUN_OCR" and values["-FOLDER1-"] != "":
+            run_ocr(values["-FOLDER1-"], values["-TEMPLATE_CHOICE-"])
             state = "WAITING"
 
     window.close()
--- template.py
+++ template.py
@@ -30,15 +30,24 @@ def is_template_name_valid(template_name):
 
 
 def read_template():
-    with open(TEMPLATE_FILENAME, "r") as f:
-        content_json = json.loads(f.read())
+    while True:
+        try:
+            with open(TEMPLATE_FILENAME, "r") as f:
+                content_json = json.loads(f.read())
+            break
+        except FileNotFoundError:
+            empty_dict = {}
+            with open(TEMPLATE_FILENAME, "x") as f:
+                json.dump(empty_dict, f)
 
     return content_json
 
 
 def write_template(content_raw):
+    content_prev = read_template()
+    content_prev.update(content_raw)
     with open(TEMPLATE_FILENAME, "w+") as f:
-        json.dump(content_raw, f, indent=4)
+        json.dump(content_prev, f, indent=4)
 
 
 def get_template_name_list():
@@ -46,3 +55,8 @@ def get_template_name_list():
         return [template_name for template_name in read_template()]
     except FileNotFoundError:
         return []
+
+
+def get_template_crop_values(template_name):
+    content = read_template()
+    return content[template_name]["Crop_Value"]
